d106378b99286762837fef548733aaab72112232
John McBride <john@johnmcbride.me>
2017-11-17 12:58:29 -0500

    Updated model to reflect the login form changes

---
+++ Models/LoginInfo.cs
@@ -0,0 +1,12 @@
+namespace Models
+{
+    public class LoginInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Domain { get; set; }
+        public bool UseSSL { get; set; }
+        public string SFAddress { get; set; }
+        public string SFWebURL { get; set; }
+    }
+}
