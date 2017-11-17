d2c92d866c69ed580abb8a2c71a99600ff7699e2
John McBride <john@johnmcbride.me>
2017-11-17 13:01:33 -0500

    implemented applist controller

--- Controllers/ApplicationsController.cs
+++ Controllers/ApplicationsController.cs
@@ -1,10 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Text;
 
 public class ApplicationsController : Controller
 {
-    [Route("Applications")]
     public IActionResult Index()
     {
-        return View();
+
+        if ( TempData.ContainsKey("applist") )
+        {
+            HttpContext.Session.SetString("applist",TempData["applist"].ToString());
+
+            var applist = JsonConvert.DeserializeObject<List<CitrixApplicationInfo>>(TempData["applist"].ToString());
+
+            return View(applist);
+        }
+        else
+        {
+            return View(new List<CitrixApplicationInfo>());
+        }
+        
     }
+
+    public async Task<IActionResult> Launch(string AppID)
+    {
+        var applist = JsonConvert.DeserializeObject<List<CitrixApplicationInfo>>(HttpContext.Session.GetString("applist"));
+
+        var application = applist.SingleOrDefault(appInfo => appInfo.ID == AppID);
+
+        string icaFile = null;
+
+        if ( application != null )
+        {
+            icaFile = await Storefront.RetreiveICA(application.Auth.StorefrontUrl,application.Auth,application);
+        }
+        ASCIIEncoding  encoding = new ASCIIEncoding();
+        byte[] bytes = Encoding.ASCII.GetBytes(icaFile);
+        
+        return File(bytes,"application/x-ica");
+        
+    }
+   
 }
